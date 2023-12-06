5ecc1c90795182d60b8bfb419df32db7ba400e0e
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-05 21:01:11 -0600

    Got time to render on load

--- script.js
+++ script.js
@@ -29,29 +29,39 @@ const showTime = () => {
         'December',
     ]
 
+    //Gets hours, minutes, seconds and sets to varaibles
     let hours = date.getHours()
     let mins = date.getMinutes()
     let secs = date.getSeconds()
+    let ms = date.getMilliseconds()
     let session = 'AM'
 
+
+    //Select the day of the week, either sunday - saturday. 
     let dayOfWeek = weekday[date.getDay()]
+    //Select the day of the month, either 1 - 31. 
     let dayOfMonth = date.getDate()
+    console.log(dayOfMonth, dayOfWeek)
 
-    let month = months.getMonth()
-
-
-
-
+    //Logic for if AM or PM
     if (hours == 0 ) {
         hours = 12
     }
 
     if (hours > 12) {
-        h = h- 12
+        hours = hours - 12
         session = 'PM'
     }
 
-    console.log(hours, mins, secs)
+    let time = `${hours}:${mins}:${secs}.${ms} ${session}`
+
+    document.querySelector(".clock").innerText = time
+    document.querySelector(".clock").textContent = time
+
+    document.querySelector()
+
+
+    console.log(time)
 
 
 
